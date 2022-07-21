a3e13453c8357508a67153cdbd71f59a5569693e
mixed_nuts_nuts <french0719@gmail.com>
2022-07-21 17:07:01 +0700

    navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const BottomTab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return null;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: "black" },
+          headerShown: false,
+          tabBarActiveTintColor: "white",
+        }}
+      >
+        <BottomTab.Screen name="home" component={HomeScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
